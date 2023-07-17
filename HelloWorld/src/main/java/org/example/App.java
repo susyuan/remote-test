78184a8652a37f5d6a59ca47370a4b6b119b2c87
susyuan <2027676594@qq.com>
2023-07-17 14:36:29 +0800

    添加 local

--- HelloWorld/src/main/java/org/example/App.java
+++ HelloWorld/src/main/java/org/example/App.java
@@ -3,5 +3,8 @@ package org.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("hello-world");
+        System.out.println("hello-world-local");
+        System.out.println("hello-world-local");
+        System.out.println("hello-world-local");
     }
 }
